fc5e0bdbd08bf00eabbe589e9b9d4e5c3799747a
trieu.ngoquang <ngoquangtrieu99tb@gmail.com>
2020-06-05 23:29:46 +0700

    test

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
 export default class App extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    )
-  }
+    render() {
+        return (
+            <View>
+                <Text> App </Text>
+            </View>
+        )
+    }
 }
--- src/common/Style.js
+++
@@ -1,9 +0,0 @@
-const Styles = {
-  rectangle: {
-    width: '90%',
-    height: 200,
-    backgroundColor: 'red',
-  },
-};
-
-export default Styles;
