b12f6f92b3bb36f6fdbf7ee7aca63e78be1bd2da
trieu.ngoquang <trieu.ngoquang@vti.com.vn>
2020-06-08 09:05:20 +0700

    fix

--- App.js
+++ App.js
@@ -1,26 +1,21 @@
-import React, { Component } from 'react'
-import { Text, View, Alert, StyleSheet } from 'react-native'
-import Button from 'react-native-button'
-import ButtonBasic from './src/components/ButtonBasic'
+import React, {Component} from 'react';
+import {Text, View, Alert, StyleSheet} from 'react-native';
+import ButtonBasic from './src/components/ButtonBasic';
 
 export default class App extends Component {
-  _onPressButton = () => {
-    Alert.alert("Click show alert !");
-  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Button</Text>
-        <ButtonBasic/>
+        <ButtonBasic />
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
-  container : {
+  container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+  },
 });
--- src/components/ButtonBasic.js
+++ src/components/ButtonBasic.js
@@ -1,25 +1,30 @@
-import React, { Component } from 'react'
-import {Text, View, TouchableOpacity, StyleSheet } from 'react-native'
-import Button from 'react-native-button'
+import React, {Component} from 'react';
+import {Alert, Text, View, TouchableOpacity, StyleSheet} from 'react-native';
+import Button from 'react-native-button';
 
 export default class ButtonBasic extends Component {
-    _onPressButton() {
-        alert('This button alert !');
-    }
-    render() {
-        return (
-            <View style={{
-                  fontSize: 20,
-                  fontWeight: 'bold',
-                  backgroundColor: 'blue',
-                  color: 'white',
-                  padding: 10,
-                  borderRadius: 10,
-                }}
-                onPress={this._onPressButton}>
-                <Button>Button Basic</Button>
-            </View>
-        )
-    }
+  _onPressButton() {
+    Alert.alert('This button alert !');
+  }
+  render() {
+    return (
+      <View>
+        <Text>Button</Text>
+        <Button onPress={this._onPressButton} style={styles.buttonBasic}>
+          Button Basic
+        </Button>
+      </View>
+    );
+  }
 }
 
+const styles = StyleSheet.create({
+  buttonBasic: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    backgroundColor: 'blue',
+    color: 'white',
+    padding: 10,
+    borderRadius: 10,
+  },
+});
