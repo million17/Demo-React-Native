9a3fabbfabf9be218b2d3c994b921070b5239a11
trieu.ngoquang <trieu.ngoquang@vti.com.vn>
2020-06-12 13:28:19 +0700

    HorizontalFlatList Data Weather

--- src/components/flatList/HorizontalFlatList.js
+++ src/components/flatList/HorizontalFlatList.js
@@ -1,72 +1,109 @@
-import React, { Component } from 'react';
-import {
-    Text,
-    View,
-    FlatList,
-    StyleSheet,
-    Image,
-    Alert,
-    Platform,
-    TouchableHighlight,
-} from 'react-native';
-import { horizontalFlatListData } from '../../data/horizontalFlatListData'
-import Icon from 'react-native-vector-icons/Ionicons'
-class HorizontalFlatListItem {
-    render() {
-        return (
-            <View style={styles.horizontalFlatListItem}>
-                <Text>{this.props.item.hour}</Text>
-                <Icon
-                    name={this.props.item.status.android}
-                    size={30}
-                    color='white'></Icon>
-                <Text>{this.props.item.degress} F</Text>
-            </View>
-        )
-    }
+import React, {Component} from 'react';
+import {Text, View, FlatList, StyleSheet, Platform, Image} from 'react-native';
+import {horizontalFlatListData} from '../../data/horizontalFlatListData';
+import {horizontalFlatListStatus} from '../../data/horizontalFlatListData';
+import Icon from 'react-native-vector-icons/Ionicons';
+class HorizontalFlatListItem extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <View style={styles.horizontalFlatListItem}>
+        <Text style={styles.textItem}>{this.props.item.hour}</Text>
+        <Icon
+          name={
+            Platform.OS === 'ios'
+              ? this.props.item.status.ios
+              : this.props.item.status.android
+          }
+          size={30}
+          color="white"
+        />
+        <Text style={styles.textItem}>{this.props.item.degress} F</Text>
+      </View>
+    );
+  }
 }
 export default class HorizontalFlatList extends Component {
-    render() {
-        return (
-            <View style={styles.containerHorizontal}>
-                <View style={styles.main}>
-                    <FlatList style={styles.mainHeader}
-                        horizontal={true}
-                        data={horizontalFlatListData}
-                        keyExtractor={(item, index) => item.hour}
-                        renderItem={({ item, index }) => {
-                            return (
-                                <HorizontalFlatListItem item={item} index={index} parentFlatList={this}>
-                                    {' '}
-                                </HorizontalFlatListItem>
-                            );
-                        }}></FlatList>
-                </View>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.containerHorizontal}>
+        <Text style={styles.textTitle}>Weather</Text>
+        <View style={styles.backgroundAbosolute}>
+          <Image
+            style={styles.imageBackground}
+            source={require('../../images/backgroundScrollView.jpg')}
+          />
+        </View>
+        <View style={styles.main}>
+          <FlatList
+            style={styles.mainHeader}
+            horizontal={true}
+            data={horizontalFlatListData}
+            keyExtractor={(item, index) => item.hour}
+            renderItem={({item, index}) => {
+              console.log(`Item : = ${JSON.stringify(item)}`);
+              return (
+                <HorizontalFlatListItem
+                  item={item}
+                  index={index}
+                  parentFlatList={this}>
+                  {' '}
+                </HorizontalFlatListItem>
+              );
+            }}
+          />
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    containerHorizontal: {
-        flexDirection: 'column',
-        flex: 1
-    },
-    main: {
-        height: 150
-    },
-    mainHeader: {
-        backgroundColor: 'black',
-        opacity: 0.5
-    },
-    horizontalFlatListItem: {
-        flex: 1,
-        flexDirection: 'column',
-        alignItems: 'center',
-        width: 90,
-        borderRadius: 10,
-        borderWidth: 1,
-        margin: 5,
-        borderColor: 'gray'
-    }
+  containerHorizontal: {
+    flexDirection: 'column',
+    flex: 1,
+  },
+  main: {
+    height: 170,
+  },
+  mainHeader: {
+    backgroundColor: 'black',
+    opacity: 0.5,
+  },
+  horizontalFlatListItem: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: 90,
+    borderRadius: 10,
+    borderWidth: 1,
+    margin: 5,
+    borderColor: 'gray',
+  },
+  textItem: {
+    color: 'white',
+    fontSize: 17,
+  },
+  textTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    padding: 5,
+  },
+  backgroundAbosolute: {
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    top: 0,
+    left: 0,
+  },
+  imageBackground: {
+    flex: 1,
+    flexDirection: 'column',
+    height: null,
+    width: null,
+    justifyContent: 'center',
+    backgroundColor: 'transparent',
+  },
 });
--- src/data/horizontalFlatListData.js
+++ src/data/horizontalFlatListData.js
@@ -1,85 +1,87 @@
-var horizontalFlatListStatus = [
-    {
-        rainy: {
-            android: 'rainy'
-        },
-        cloud: {
-            android: 'cloud'
-        },
-        suny: {
-            android: 'suny'
-        },
-        thunderstorm: {
-            android: 'thunderstorm'
-        },
-        flash: {
-            android: 'flash'
-        }
-    }
-
-];
+var horizontalFlatListStatus = {
+  rainy: {
+    android: 'md-rainy',
+    ios: 'ios-rainy',
+  },
+  cloud: {
+    android: 'md-cloud',
+    ios: 'ios-cloud',
+  },
+  suny: {
+    android: 'md-suny',
+    ios: 'ios-suny',
+  },
+  thunderstorm: {
+    android: 'md-thunderstorm',
+    ios: 'ios-thunderstorm',
+  },
+  flash: {
+    android: 'md-flash',
+    ios: 'ios-flash',
+  },
+};
 
 var horizontalFlatListData = [
-    {
-        hour: "1 Am",
-        status: horizontalFlatListStatus.rainy,
-        degress: 20,
-    },
-    {
-        hour: "2 Am",
-        status: horizontalFlatListStatus.flash,
-        degress: 20,
-    },
-    {
-        hour: "3 Am",
-        status: horizontalFlatListStatus.flash,
-        degress: 20,
-    }
-    ,
-    {
-        hour: "4 Am",
-        status: horizontalFlatListStatus.flash,
-        degress: 32,
-    },
-    {
-        hour: "5 Am",
-        status: horizontalFlatListStatus.flash,
-        degress: 12,
-    },
-    {
-        hour: "6 Am",
-        status: horizontalFlatListStatus.thunderstorm,
-        degress: 23,
-    },
-    {
-        hour: "7 Am",
-        status: horizontalFlatListStatus.thunderstorm,
-        degress: 43,
-    }, {
-        hour: "8 Am",
-        status: horizontalFlatListStatus.rainy,
-        degress: 12,
-    },
-    {
-        hour: "9 Am",
-        status: horizontalFlatListStatus.rainy,
-        degress: 33,
-    },
-    {
-        hour: "10 Am",
-        status: horizontalFlatListStatus.cloud,
-        degress: 22,
-    },
-    {
-        hour: "11 Am",
-        status: horizontalFlatListStatus.cloud,
-        degress: 60,
-    },
-    {
-        hour: "12 Am",
-        status: horizontalFlatListStatus.cloud,
-        degress: 30,
-    }
+  {
+    hour: '1 Am',
+    status: horizontalFlatListStatus.rainy,
+    degress: 20,
+  },
+  {
+    hour: '2 Am',
+    status: horizontalFlatListStatus.flash,
+    degress: 20,
+  },
+  {
+    hour: '3 Am',
+    status: horizontalFlatListStatus.flash,
+    degress: 20,
+  },
+  {
+    hour: '4 Am',
+    status: horizontalFlatListStatus.flash,
+    degress: 32,
+  },
+  {
+    hour: '5 Am',
+    status: horizontalFlatListStatus.flash,
+    degress: 12,
+  },
+  {
+    hour: '6 Am',
+    status: horizontalFlatListStatus.thunderstorm,
+    degress: 23,
+  },
+  {
+    hour: '7 Am',
+    status: horizontalFlatListStatus.thunderstorm,
+    degress: 43,
+  },
+  {
+    hour: '8 Am',
+    status: horizontalFlatListStatus.rainy,
+    degress: 12,
+  },
+  {
+    hour: '9 Am',
+    status: horizontalFlatListStatus.rainy,
+    degress: 33,
+  },
+  {
+    hour: '10 Am',
+    status: horizontalFlatListStatus.cloud,
+    degress: 22,
+  },
+  {
+    hour: '11 Am',
+    status: horizontalFlatListStatus.cloud,
+    degress: 60,
+  },
+  {
+    hour: '12 Am',
+    status: horizontalFlatListStatus.cloud,
+    degress: 30,
+  },
 ];
-export { horizontalFlatListData };
-export { horizontalFlatListStatus };
+export {horizontalFlatListData};
+export {horizontalFlatListStatus};
